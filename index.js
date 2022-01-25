f2a45bceac0030556f7d33574bef2400117430fa
Milan <jmilan.97@gmail.com>
2022-01-26 00:27:49 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const fs = require("fs");
+const URL = require("url");
+
+http.createServer((req, res) => {
+    
+    let path = req.url;
+    switch(path){
+        case "/":
+            fs.readFile("./index.html", (err, data) => {
+                if(err){console.log(err)}
+                else{
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    res.write(data);
+                    res.end();
+                };
+            });
+            break;
+
+        case "/about":
+            fs.readFile("./about.html", (err, data) => {
+                if(err){console.log(err)}
+                else{
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    res.write(data);
+                    res.end();
+                };
+            });
+            break;
+        
+        case "/contact-me":
+            fs.readFile("./contact-me.html", (err, data) => {
+                if(err){console.log(err)}
+                else{
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    res.write(data);
+                    res.end();
+                };
+            });
+            break;
+        default:
+            fs.readFile("./404.html", (err, data) => {
+                if(err){console.log(err)}
+                else{
+                    res.writeHead(404, {"Content-Type": "text/html"});
+                    res.write(data);
+                    res.end();
+                };
+            });
+            break;
+    }
+
+}).listen(8080);

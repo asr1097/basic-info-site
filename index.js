07a7ffa0196a31ab5cd84cdf4534b61b9783926a
Milan <jmilan.97@gmail.com>
2022-01-26 15:19:54 +0100

    new commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { URL } = require("url");
 http.createServer((req, res) => {
     let urlString = "http://" + req.headers.host + req.url;
     let url = new URL(urlString);
+    console.log(url)
     switch(url.pathname){
         case "/":
             fs.readFile("./index.html", (err, data) => {
